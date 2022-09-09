b70bbd8295cc1e6f5bc2978c9a664d74b7f57b92
DragonSky_NG <dragonskyng@gmail.com>
2022-09-09 11:36:03 +0300

    Create personController.js skeleton

---
+++ controller/personController.js
@@ -0,0 +1,33 @@
+import personModel from "../models/personModel";
+
+
+export const createPerson = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const getPeople = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const getPersonById = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const updatePersonById = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const deletePersonById = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+};
--- routes/personRoute.js
+++ routes/personRoute.js
@@ -1,8 +1,15 @@
 import express from "express";
+import {
+  createPerson,
+  getPeople,
+  getPersonById,
+  updatePersonById,
+  deletePersonById,
+} from "../controller/personController";
 const router = express.Router();
 
 router.post("/addNew", createPerson);
-router.get("/getAll", getPerson);
+router.get("/getAll", getPeople);
 router.get("/get/:id", getPersonById);
 router.put("/update/:id", updatePersonById);
 router.delete("/delete/:id", deletePersonById);

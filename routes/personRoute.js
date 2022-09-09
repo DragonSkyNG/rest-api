e734877bf2817e6ef16af3dcc06ffb1e7bd457b3
DragonSky_NG <dragonskyng@gmail.com>
2022-09-09 11:03:09 +0300

    Add routes to personRoute.js

--- routes/personRoute.js
+++ routes/personRoute.js
@@ -1,6 +1,10 @@
 import express from "express";
 const router = express.Router();
 
+router.post("/addNew", createPerson);
+router.get("/getAll", getPerson);
+router.get("/get/:id", getPersonById);
+router.put("/update/:id", updatePersonById);
+router.delete("/delete/:id", deletePersonById);
 
-
-export default router
+export default router;

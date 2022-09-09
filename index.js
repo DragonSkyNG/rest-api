b10bf6dd1413a104eb6a1300d46a93d3e9a1e11e
DragonSky_NG <dragonskyng@gmail.com>
2022-09-09 10:54:41 +0300

    Add person and 404 routes

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import personRoute from "./routes/personRoute.js";
 
 const app = express();
 
-dotenv.config()
+dotenv.config();
 
 app.use(express.json);
 
-const connectToDB = async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("Connected to mongodb cluster successfully");
-    } catch (error) {
-        console.error(error)
-    }
-}
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to mongodb cluster successfully");
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-app.get("/",(req,res)=>{
-    res.send("hi");
-})
+app.use("/person", personRoute);
+
+app.get("*", (req, res) => {
+  res.status(404).send("This page doesn't exist");
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, ()=>{
-    connectToDB();
-    console.log(`server started on port: ${port}`);
+app.listen(port, () => {
+  connectToDB();
+  console.log(`server started on port: ${port}`);
 });
